cf9289167486fc885db043f361d1f3e903289fd4
rassi0429 <taitokokoa+rassi@gmail.com>
2022-06-04 18:28:01 +0900

    fix: endpoint

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export const state = () => ({
   version: '1.0.0:' + process.env.buildTime,
   buildTime: process.env.buildTime,
-  endpoint: process.env.api_host || "http://localhost:3080"
+  endpoint: process.env.api_host || "https://photo-api.neos.love"
 })
