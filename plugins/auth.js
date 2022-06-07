e577479470d3de4d265c48e0675ee3db51e3d8c3
rassi0429 <taitokokoa+rassi@gmail.com>
2022-06-08 05:14:53 +0900

    fix: comment

--- plugins/auth.js
+++ plugins/auth.js
@@ -2,9 +2,7 @@ import firebase from '~/plugins/firebase'
 
 const auth = () => {
   return new Promise((resolve) => {
-    // console.log('plugins/auth')
     firebase.auth().onAuthStateChanged((user) => {
-      // console.log(user.getIdToken())
       resolve(user || false)
     })
   })
