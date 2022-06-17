93f87aa8bfea75a866344f6c12e1a8f064835bca
rassi0429 <taitokokoa+rassi@gmail.com>
2022-06-17 19:09:43 +0900

    feat: docker

--- back/backup.sh
+++ back/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 date=`date +%Y%m%d-%H`
-mysqldump --skip-column-statistics --single-transaction --quick --lock-tables=false -h 127.0.0.1 --port 3388 -u neos -ppassword photo | xz -3 > backups/${date}.sql.xz
+mysqldump --skip-column-statistics --single-transaction --quick --lock-tables=false -h 127.0.0.1 --port 3388 -u neos -ppassword photo | xz -3 > /home/neo/NeosProject/photo.neos.love/back/backups/${date}.sql.xz
