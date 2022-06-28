99e0ad2343bc80bf97da0ebfaf9d6b9282b595b4
rassi0429 <taitokokoa+rassi@gmail.com>
2022-06-29 04:44:43 +0900

    fix: modal

--- front/store/modal.js
+++ front/store/modal.js
@@ -1,4 +1,3 @@
-
 import axios from "axios";
 import auth from "@/plugins/auth";
 
@@ -20,26 +19,30 @@ export const state = () => ({
 export const getters = {}
 
 export const actions = {
-  async updatePhoto({commit, state,rootState}) {
+  async updatePhoto({commit, state, rootState}) {
     const user = await auth()
-    if(user) {
+    if (user) {
       const token = await user.getIdToken(true)
-      await axios.post(`${rootState.endpoint}/v1/photo/${state.modalData.id}`,{comment:state.modalData.comment, tags: state.editingTag},{headers: {token}})
+      await axios.post(`${rootState.endpoint}/v1/photo/${state.modalData.id}`, {
+        comment: state.modalData.comment,
+        tags: state.editingTag
+      }, {headers: {token}})
       const {data} = await axios.get(`${rootState.endpoint}/v1/photo/${state.modalData.id}`)
       commit('openModal', data)
     }
   },
-  async updateData({commit,state,rootState}) {
+  async updateData({commit, state, rootState}) {
     try {
       const {data} = await axios.get(`${rootState.endpoint}/v1/photo/${state.modalData.id}`)
       commit('openModal', data)
-    } catch {}
+    } catch {
+    }
   },
-  async deletePhoto({commit,state,rootState}) {
+  async deletePhoto({commit, state, rootState}) {
     const res = confirm("削除してもいいですか");
-    if(res) {
+    if (res) {
       const user = await auth()
-      if(user) {
+      if (user) {
         const token = await user.getIdToken(true)
         await axios.delete(`${rootState.endpoint}/v1/photo/${state.modalData.id}`, {headers: {token}})
         await this.$router.replace({'query': null});
@@ -51,19 +54,19 @@ export const actions = {
 
 export const mutations = {
   openModal(state, data) {
-    if(state.isModalOpen) return
+    if (state.isModalOpen) return
 
     const nfswTags = ['nfsw', 'r18'];
     const name = data.tags.map(t => t.name)
-    if(name.includes(nfswTags[0]) || name.includes(nfswTags[1])) {
+    if (name.includes(nfswTags[0]) || name.includes(nfswTags[1])) {
       const useraction = window.confirm("エッチな画像が含まれます。あなたは18歳以上ですか？")
-      if(!useraction) {
+      if (!useraction) {
         window.location = "https://kids.yahoo.co.jp/"
-      } else {
-        state.isModalOpen = true
-        state.modalData = data
-        this.$router.replace({'query': {modal: data.id}});
       }
+    } else {
+      state.isModalOpen = true
+      state.modalData = data
+      this.$router.replace({'query': {modal: data.id}});
     }
 
   },
@@ -80,22 +83,22 @@ export const mutations = {
     state.isEditing = false
     state.editingTag = []
   },
-  updateComment(state,data) {
+  updateComment(state, data) {
     state.modalData.comment = data
   },
-  updateTags(state,data) {
+  updateTags(state, data) {
     state.editingTag = data
   },
-  updateEditState(state,data) {
+  updateEditState(state, data) {
     state.isEditing = data
-    if(data) {
+    if (data) {
       state.editingTag = state.modalData.tags.map(t => t.name)
     }
   },
-  deleteTag(state,data) {
+  deleteTag(state, data) {
     state.editingTag.splice(data, 1)
   },
-  setWidth(state,data) {
+  setWidth(state, data) {
     state.width = data
   }
 }
